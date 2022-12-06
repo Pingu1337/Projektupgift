2457ed3039494fa4ba8ceb03fd2493c018ff98bd
mathisson <victor.mathisson@icloud.com>
2022-12-06 13:54:45 +0100

    Added Authorization to UrlShortener and Jwt Token Support

---
+++ LoginService/LoginContext.cs
@@ -0,0 +1,11 @@
+﻿using LoginService.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginService
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options) : base(options) {}
+        public DbSet<User> Users => Set<User>();
+    }
+}
